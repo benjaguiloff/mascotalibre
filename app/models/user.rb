14c10119aee8488d751e01f31f5c8a525f46ea26
Sebastian Ramos <ramos.sebastian1@uc.cl>
2021-05-04 22:31:49 -0300

    crud usuario

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_attached_file :image, styles: { medium: '200x200>', thumb: '48x48>' }
+end
