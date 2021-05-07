940851a1e7cf9a0f110372cacc6b4620e5ce20bb
Sebastian Ramos <ramos.sebastian1@uc.cl>
2021-05-06 22:53:42 -0300

    test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -14,3 +14,11 @@ RSpec.describe "Users", type: :request do
   end
 end
 
+RSpec.describe UsersController, :type => :controller do
+  describe "GET index" do
+    it "has a 200 status code" do
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
+end
