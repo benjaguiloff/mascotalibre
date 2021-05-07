3396ba97f26786dcd4bdfdcd6fcfa1a6b6f94916
benjaguiloff <byguiloff@uc.cl>
2021-05-06 21:06:32 -0400

    reseñas listas

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,45 @@
+class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+    @users = User.all
+  end
+
+  def show
+    @users = User.all
+    @review = Review.find(params[:id])
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review_params = params.require(:review).permit(:content, :id_user, :id_reviewed)
+    @review = Review.create(@review_params)
+    if @review.save
+      redirect_to reviews_index_path, notice: 'Reseña creada correctamente'
+    else
+      redirect_to reviews_new_path, notice: 'Error al crear reseña'
+    end
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @reviews_params = params.require(:review).permit(:content, :id_user, :id_reviewed)
+    @review = Review.find(params[:id])
+    if @review.update(@reviews_params)
+      redirect_to reviews_index_path, notice: 'Reseña actualizada correctamente'
+    else
+      redirect_to reviews_edit_path(@review.id), notice: 'Error al actualizar la reseña'
+    end
+  end
+
+  def delete
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to reviews_index_path, notice: 'Reseña eliminada'
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_203925) do
+ActiveRecord::Schema.define(version: 2021_05_06_211012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 2021_05_05_203925) do
     t.string "email_emisor"
     t.string "email_receptor"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "id_user"
+    t.integer "id_reviewed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
