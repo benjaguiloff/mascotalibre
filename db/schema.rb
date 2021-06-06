3c853eed8666ca961518c6a3d9a15dbb9931256a
benjaguiloff <byguiloff@uc.cl>
2021-06-06 17:26:08 -0400

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_06_04_210353) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,13 +68,15 @@ ActiveRecord::Schema.define(version: 2021_06_04_210353) do
     t.datetime "updated_at", null: false
   end
 
-
-  create_table "fakes", force: :cascade do |t|
-    t.string "accepted"
-
   create_table "conversations", force: :cascade do |t|
     t.integer "sender_id"
     t.integer "recipient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "fakes", force: :cascade do |t|
+    t.string "accepted"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -98,7 +98,7 @@ ActiveRecord::Schema.define(version: 2021_06_04_210353) do
     t.string "typo"
     t.integer "price"
     t.text "direction"
-    t.string "accepted", default: "no"
+    t.string "accepted", default: "false"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
