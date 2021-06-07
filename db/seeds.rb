905bbd94e66e20f7ef9ae900e80fb172d5af5602
benjaguiloff <byguiloff@uc.cl>
2021-06-06 21:08:07 -0400

    fix admin para heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
-Fake.create(accepted: "false") if Rails.env.development?
-Fake.create(accepted: "true") if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+Fake.create(accepted: "false")
+Fake.create(accepted: "true")
